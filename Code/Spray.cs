5caa6a4774175cb1d4227299aaa5283025c52f56
badandbest <91832803+badandbest@users.noreply.github.com>
2024-07-24 17:28:18 +0200

    Refactor

--- Code/Spray.cs
+++ Code/Spray.cs
@@ -1,19 +1,24 @@
 using Sandbox;
-using System.Linq;
+using Sandbox.Utility;
 
 namespace badandbest.Sprays;
 
+/// <summary>
+/// A library to allow the placement of spray in the world
+/// </summary>
 public static class Spray
 {
-	[ConVar( "spray", Help = "The image you want to spray.", Saved = true )]
-	static string SprayUrl { get; set; }
-	
+	[ConVar( "spray", Help = "URL of image. Must be in quotes.", Saved = true )]
+	private static string ImageUrl { get; set; } = "materials/decals/default.png";
+
+	private static GameObject LocalSpray;
+
 	/// <summary>
 	/// Places an image on a surface.
 	/// </summary>
 	public static void Place()
 	{
-		const float RANGE = 128; // Range in GMOD.
+		const float RANGE = 128;// Range in GMOD.
 
 		var ray = Game.ActiveScene.Camera.Transform.World.ForwardRay;
 		var trace = Game.SceneTrace.Ray( ray, RANGE );
@@ -27,40 +32,44 @@ public static class Spray
 	/// <param name="trace">The trace to use.</param>
 	public static void Place( SceneTrace trace )
 	{
-		var tr = trace.Run();
-		if ( !tr.Hit )
-		{
+		// We only want to hit static bodies. ( maps, etc )
+		if ( trace.Run() is not { Body.BodyType: PhysicsBodyType.Static } tr )
 			return;
-		}
 
-		var sprayRenderer = Game.ActiveScene.GetAllComponents<SprayRenderer>().SingleOrDefault( x => x.Network.IsOwner );
-
-		if ( !sprayRenderer.IsValid() )
+		var config = new CloneConfig
 		{
-			var sprayObj = SceneUtility.GetPrefabScene( ResourceLibrary.Get<PrefabFile>( "spray.prefab" ) ).Clone();
-			sprayObj.NetworkSpawn();
-			sprayRenderer = sprayObj.Components.Get<SprayRenderer>();
-		}
+			Name = $"Spray - {Steam.PersonaName}",
+			Transform = new( tr.HitPosition, Rotation.LookAt( tr.Normal ) ),
+			PrefabVariables = new() { { "Image", ImageUrl } }
+		};
 
-		Sound.Play( "SprayCan.Paint", tr.HitPosition );
+		LocalSpray?.Destroy();
+		LocalSpray = GameObject.Clone( "prefabs/spray.prefab", config );
 
-		sprayRenderer.Transform.World = new Transform( tr.HitPosition, Rotation.LookAt( tr.Normal ) );
-		sprayRenderer.Image = SprayUrl;
+		LocalSpray.NetworkSpawn();// NetworkSpawn breaks the prefab
+		LocalSpray.SetPrefabSource( "prefabs/spray.prefab" );
 	}
 }
 
 [Title( "Spray Renderer" ), Icon( "imagesearch_roller" )]
-internal class SprayRenderer : Renderer
+file class SprayRenderer : Renderer
 {
-	[Property, ImageAssetPath, Sync, Facepunch.Change( nameof( OnImageChanged ) )]
+	[Property, ImageAssetPath]
 	public string Image { get; set; }
 
-	public async void OnImageChanged( string oldValue, string newValue )
+	protected override async void OnEnabled()
 	{
-		var decal = Components.GetInChildren<DecalRenderer>().Material;
+		var texture = await Texture.LoadAsync( null, Image, false );
 
-		// Clear texture while next image is downloading.
-		decal.Set( "g_tColor", Texture.Transparent );
-		decal.Set( "g_tColor", await Texture.LoadAsync( null, newValue ) );
+		if ( texture is null or { Width: <= 32, Height: <= 32 } )
+		{
+			// Probably an error texture. Replace with a fallback image.
+			texture = Texture.Load( FileSystem.Mounted, "materials/fallback.png" );
+		}
+
+		var material = Material.Load( "materials/spray.vmat" ).CreateCopy();
+		material.Set( "g_tColor", texture );
+
+		Components.Get<DecalRenderer>( FindMode.InChildren ).Material = material;
 	}
 }
